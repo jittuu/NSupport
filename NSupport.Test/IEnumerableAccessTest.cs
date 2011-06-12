1d077eefa9bf71f669b9e24e7084f04a30a17e50
Soe Moe <soemoetunlwin@gmail.com>
2011-06-12 22:00:11 +0800

    added From method for IEnumearble

---
+++ NSupport.Test/IEnumerableAccessTest.cs
@@ -0,0 +1,35 @@
+﻿namespace NSupport.Test {
+    using Xunit;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class IEnumerableAccessTest {
+        [Fact]
+        public void Test_From_with_index_0() {
+            var array = new string[] { "a", "b", "c", "d" }.From(0);
+
+            Assert.Equal(4, array.Count());
+            Assert.Single(array, "a");
+            Assert.Single(array, "b");
+            Assert.Single(array, "c");
+            Assert.Single(array, "d");
+        }
+
+        [Fact]
+        public void Test_From_in_range_index() {
+            var array = new string[] { "a", "b", "c", "d" }.From(2);
+
+            Assert.Equal(2, array.Count());
+            Assert.DoesNotContain("a", array);
+            Assert.DoesNotContain("b", array);
+            Assert.Single(array, "c");
+            Assert.Single(array, "d");
+        }
+
+        [Fact]
+        public void Test_From_out_range_index() {
+            var array = new string[] { "a", "b", "c", "d" }.From(4);
+            Assert.Empty(array);
+        }
+    }
+}
---
+++ NSupport/IEnumerableAccess.cs
@@ -0,0 +1,18 @@
+﻿namespace NSupport {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public static class IEnumerableAccess {
+        public static IEnumerable<T> From<T>(this IEnumerable<T> source, int index) {
+            int localIndex = 0;
+
+            foreach (var item in source) {
+                if (localIndex++ >= index) {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
