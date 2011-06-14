e155cdfababf2b8819ef336d278d35bfeabdec65
Soe Moe <soemoetunlwin@gmail.com>
2011-06-14 12:04:27 +0800

    added DateTime.Tomorrow

---
+++ NSupport.Test/DateTimeCalculationsTest.cs
@@ -0,0 +1,14 @@
+﻿namespace NSupport.Test {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Xunit;
+
+    public class DateTimeCalculationsTest {
+        [Fact]
+        public void Test_Tomorrow() {
+            Assert.Equal(new DateTime(2010, 6, 15), new DateTime(2010, 6, 14).Tomorrow());
+        }
+    }
+}
---
+++ NSupport/DateTimeCalculations.cs
@@ -0,0 +1,12 @@
+﻿namespace NSupport {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public static class DateTimeCalculations {
+        public static DateTime Tomorrow(this DateTime source) {
+            return source.AddDays(1);
+        }
+    }
+}
