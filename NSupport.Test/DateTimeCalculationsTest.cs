dd622478c93874a6fb022b19f757bf30f45a2f1e
Soe Moe <soemoetunlwin@gmail.com>
2011-06-14 12:09:46 +0800

    added more asserts for Tomorrow

--- NSupport.Test/DateTimeCalculationsTest.cs
+++ NSupport.Test/DateTimeCalculationsTest.cs
@@ -9,6 +9,8 @@
         [Fact]
         public void Test_Tomorrow() {
             Assert.Equal(new DateTime(2010, 6, 15), new DateTime(2010, 6, 14).Tomorrow());
+            Assert.Equal(new DateTime(2010, 6, 1), new DateTime(2010, 5, 31).Tomorrow()); // between months
+            Assert.Equal(new DateTime(2011, 1, 1), new DateTime(2010, 12, 31).Tomorrow()); // between years
         }
 
         [Fact]
