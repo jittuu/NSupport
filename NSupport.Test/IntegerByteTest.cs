cfe07158a9aa2328137a3b7e9de5fdebd53799f7
Soe Moe <soemoetunlwin@gmail.com>
2011-06-16 18:29:51 +0800

    added int.Byte/s()

---
+++ NSupport.Test/IntegerByteTest.cs
@@ -0,0 +1,15 @@
+﻿namespace NSupport.Test {
+    using Xunit;
+
+    public class IntegerByteTest {
+        [Fact]
+        public void Test_Byte() {
+            Assert.Equal(1, 1.Byte());
+        }
+
+        [Fact]
+        public void Test_Bytes() {
+            Assert.Equal(10, 10.Bytes());
+        }
+    }
+}
---
+++ NSupport/IntegerByte.cs
@@ -0,0 +1,12 @@
+﻿namespace NSupport {
+
+    public static class IntegerByte {
+        public static int Byte(this int source) {
+            return source.Bytes();
+        }
+
+        public static int Bytes(this int source) {
+            return source;
+        }
+    }
+}
