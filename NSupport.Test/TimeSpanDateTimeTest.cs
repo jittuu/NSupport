d4d45028d117694102db6c6908700be975f06d46
Soe Moe <soemoetunlwin@gmail.com>
2011-06-16 18:09:42 +0800

    added TimeSpan.Ago() which allows to write 1.Hours().Ago() by combining with IntegerTime

---
+++ NSupport.Test/TimeSpanDateTimeTest.cs
@@ -0,0 +1,17 @@
+﻿namespace NSupport.Test {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Xunit;
+
+    public class TimeSpanDateTimeTest {
+        [Fact]
+        public void Test_Ago() {            
+            Assert.Equal(DateTime.Now.AddSeconds(-1), new TimeSpan(0, 0, 1).Ago()); // seconds
+            Assert.Equal(DateTime.Now.AddMinutes(-1), new TimeSpan(0, 1, 0).Ago()); // minutes
+            Assert.Equal(DateTime.Now.AddHours(-1), new TimeSpan(1, 0, 0).Ago()); // hourss
+            Assert.Equal(DateTime.Now.AddDays(-1), new TimeSpan(1, 0, 0, 0).Ago()); // days            
+        }
+    }
+}
---
+++ NSupport/TimeSpanDateTime.cs
@@ -0,0 +1,12 @@
+﻿namespace NSupport {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public static class TimeSpanDateTime {
+        public static DateTime Ago(this TimeSpan source) {
+            return DateTime.Now.Subtract(source);
+        }
+    }
+}
