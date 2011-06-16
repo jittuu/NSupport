634e2b177783185402278a45b6da42f9b1e07964
Soe Moe <soemoetunlwin@gmail.com>
2011-06-16 17:17:38 +0800

    added int.IsOdd()

---
+++ NSupport.Test/IntegerEvenOddTest.cs
@@ -0,0 +1,21 @@
+﻿namespace NSupport.Test {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Xunit;
+
+    public class IntegerEvenOddTest {
+        [Fact]
+        public void Test_IsOdd() {
+            Assert.Equal(true, 1.IsOdd());
+            Assert.Equal(true, 13.IsOdd());
+            Assert.Equal(true, (-1).IsOdd());
+
+            Assert.Equal(false, 2.IsOdd());
+            Assert.Equal(false, 0.IsOdd());
+            Assert.Equal(false, 24.IsOdd());
+            Assert.Equal(false, (-4).IsOdd());
+        }
+    }
+}
---
+++ NSupport/IntegerEvenOdd.cs
@@ -0,0 +1,12 @@
+﻿namespace NSupport {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public static class IntegerEvenOdd {
+        public static bool IsOdd(this int source) {
+            return source % 2 != 0;
+        }
+    }
+}
