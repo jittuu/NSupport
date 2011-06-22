2d3fcccb9606be7c07040d0df07ea3a56c359801
Soe Moe <soemoetunlwin@gmail.com>
2011-06-22 14:51:29 +0800

    added xml documentation for EnumerableAccess

--- NSupport/EnumerableAccess.cs
+++ NSupport/EnumerableAccess.cs
@@ -3,10 +3,24 @@
     using System.Linq;
 
     public static class EnumerableAccess {
+        /// <summary>
+        /// Returns the tail of the element sequence from position.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <param name="source">An <see cref="IEnumerable{T}"/> to return elements from.</param>
+        /// <param name="index">The index no to start from.</param>
+        /// <returns>Returns the tail of the element sequence from position.</returns>
         public static IEnumerable<T> From<T>(this IEnumerable<T> source, int index) {
             return source.Skip(index);
         }
 
+        /// <summary>
+        /// Returns the beginning of the array up to position.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <param name="source">An <see cref="IEnumerable{T}"/> to return elements from.</param>
+        /// <param name="index">The index no to stop at.</param>
+        /// <returns>Returns the beginning of the array up to position.</returns>
         public static IEnumerable<T> To<T>(this IEnumerable<T> source, int index) {
             return source.Take(++index);
         }
