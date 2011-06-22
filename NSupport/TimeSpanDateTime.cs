47582552fa3c971033b85a2d77472637b8596b9e
Soe Moe <soemoetunlwin@gmail.com>
2011-06-22 16:50:10 +0800

    added xml doc for TimeSpan DateTime conversion

--- NSupport/TimeSpanDateTime.cs
+++ NSupport/TimeSpanDateTime.cs
@@ -2,18 +2,40 @@
     using System;
 
     public static class TimeSpanDateTime {
+        /// <summary>
+        /// Returns a <see cref="DateTime"/> which subtract current <see cref="TimeSpan"/> ago from now.
+        /// </summary>
+        /// <param name="source">A <see cref="TimeSpan"/> instance.</param>
+        /// <returns>Returns a <see cref="DateTime"/> which subtract current <see cref="TimeSpan"/> ago from now.</returns>
         public static DateTime Ago(this TimeSpan source) {
             return source.Until(DateTime.Now);
         }
 
+        /// <summary>
+        /// Returns a <see cref="DateTime"/> which add current <see cref="TimeSpan"/> since from now.
+        /// </summary>
+        /// <param name="source">A <see cref="TimeSpan"/> instance.</param>
+        /// <returns>Returns a <see cref="DateTime"/> which add current <see cref="TimeSpan"/> since from now.</returns>
         public static DateTime FromNow(this TimeSpan source) {
             return source.Since(DateTime.Now);
         }
 
+        /// <summary>
+        /// Returns a <see cref="DateTime"/> which subtract current <see cref="TimeSpan"/> ago from the <paramref name="time"/>.
+        /// </summary>
+        /// <param name="source">A <see cref="TimeSpan"/> instance.</param>
+        /// <param name="time">A <see cref="DateTime"/> ago from.</param>
+        /// <returns>Returns a <see cref="DateTime"/> which subtract current <see cref="TimeSpan"/> ago from the <paramref name="time"/>.</returns>
         public static DateTime Until(this TimeSpan source, DateTime time) {
             return time.Subtract(source);
         }
 
+        /// <summary>
+        /// Returns a <see cref="DateTime"/> which add current <see cref="TimeSpan"/> since from the <paramref name="time"/>.
+        /// </summary>
+        /// <param name="source">A <see cref="TimeSpan"/> instance.</param>
+        /// <param name="time">A <see cref="DateTime"/> since from.</param>
+        /// <returns>Returns a <see cref="DateTime"/> which add current <see cref="TimeSpan"/> since from the <paramref name="time"/>.</returns>
         public static DateTime Since(this TimeSpan source, DateTime time) {
             return time.Add(source);
         }
