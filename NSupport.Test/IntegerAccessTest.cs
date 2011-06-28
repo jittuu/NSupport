c7cb46bec7d7503a974a5c555623b79a69006908
Soe Moe <soemoetunlwin@gmail.com>
2011-06-29 00:48:18 +0800

    added int.Times method in integerAccess

---
+++ NSupport.Test/IntegerAccessTest.cs
@@ -0,0 +1,28 @@
+﻿namespace NSupport.Test {
+    using Xunit;
+    using System;
+
+    public class IntegerAccessTest {
+        [Fact]
+        public void Test_Integer_Times_with_null() {
+            Assert.DoesNotThrow(() => { 5.Times((Action)null); });
+            Assert.DoesNotThrow(() => { 5.Times((Action<int>)null); });
+        }
+
+        [Fact]
+        public void Test_Integer_Times_without_index() {
+            var count = 0;
+            5.Times(() => count++);
+            Assert.Equal(5, count);
+        }
+
+        [Fact]
+        public void Test_Integer_Times_with_index() {
+            var index = 0;
+            5.Times((i) => {
+                Assert.Equal(index, i);
+                index++;
+            });
+        }
+    }
+}
---
+++ NSupport/IntegerAccess.cs
@@ -0,0 +1,18 @@
+﻿namespace NSupport {    
+    using System;
+
+    public static class IntegerAccess {
+        private static Action emptyAction = (() => { });
+        private static Action<int> emptyIndexAction = ((i) => { });
+
+        public static void Times(this int source, Action action) {            
+            source.Times((i) => { (action ?? emptyAction)(); });
+        }
+
+        public static void Times(this int source, Action<int> action) {            
+            for (int i = 0; i < source; i++) {
+                (action ?? emptyIndexAction)(i);
+            }
+        }
+    }
+}
