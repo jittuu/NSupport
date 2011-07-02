b088d469b994228c0d71fb03a81620dac970ff33
Soe Moe <soemoetunlwin@gmail.com>
2011-07-02 21:10:53 +0800

    added string.FormatWith method

---
+++ NSupport.Test/StringFormattingTest.cs
@@ -0,0 +1,19 @@
+﻿namespace NSupport.Test {
+    using System;
+    using Xunit;
+    using System.Threading;
+    using System.Globalization;
+    
+    public class StringFormattingTest {
+        [Fact]
+        public void Test_FormatWith() {
+            Assert.Equal("This is formatting integer 1 with {1:00}: 02", "This is formatting integer {0} with {{1:00}}: {1:00}".FormatWith(1, 2));
+        }
+
+        [Fact]
+        public void Test_FormatWith_with_culture() {
+            var july15 = new DateTime(2011, 7, 15);
+            Assert.Equal("Date in ja-JP: 2011/07/15", "Date in ja-JP: {0:d}".FormatWith(new CultureInfo("ja-JP"), july15));
+        }
+    }
+}
---
+++ NSupport/StringFormatting.cs
@@ -0,0 +1,33 @@
+﻿namespace NSupport {
+    using System;
+    using System.Threading;
+
+    /// <summary>
+    /// Provides extension methods for <see cref="string"/> for fomatting.
+    /// </summary>
+    public static class StringFormatting {
+        /// <summary>
+        /// Replaces one or more format items in a specified string with the string representation of a specified object.
+        /// This method uses <see cref="String.Format(string, object[])"/> method intenally.
+        /// </summary>
+        /// <param name="source">A <see cref="string"/> instance.</param>
+        /// <param name="args">An object array that contains zero or more objects to format. </param>
+        /// <returns>A copy of <paramref name="source"/> in which the format items have been replaced by the string representation of the corresponding objects in <paramref name="args"/>.</returns>
+        public static string FormatWith(this string source, params object[] args) {
+            return source.FormatWith(Thread.CurrentThread.CurrentCulture, args);
+        }
+
+        /// <summary>
+        /// Replaces one or more format items in a specified string with the string representation of a specified object.
+        /// This method uses <see cref="String.Format(IFormatProvider, string, object[])"/> method intenally.
+        /// </summary>
+        /// <param name="source">A <see cref="string"/> instance.</param>
+        /// <param name="provider">An object that supplies culture-specific formatting information.</param>
+        /// <param name="args">An object array that contains zero or more objects to format. </param>
+        /// <returns>A copy of <paramref name="source"/> in which the format items have been replaced by the string representation of the corresponding objects in <paramref name="args"/>.</returns>
+        public static string FormatWith(this string source, IFormatProvider provider, params object[] args) {
+            return String.Format(provider, source, args);
+
+        }
+    }
+}
