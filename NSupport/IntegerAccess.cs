ea7f90e46cbfc11bb276142b28b7a3fd33d92c09
Soe Moe <soemoetunlwin@gmail.com>
2011-07-02 20:04:53 +0800

    add xml doc for int.Times method

--- NSupport/IntegerAccess.cs
+++ NSupport/IntegerAccess.cs
@@ -1,14 +1,27 @@
 ﻿namespace NSupport {    
     using System;
 
+    /// <summary>
+    /// Provides extension methods for <see cref="int"/> for easy access.
+    /// </summary>
     public static class IntegerAccess {
         private static Action emptyAction = (() => { });
         private static Action<int> emptyIndexAction = ((i) => { });
 
+        /// <summary>
+        /// Loops the current <see cref="int" /> times with the given <paramref name="action"/>.
+        /// </summary>
+        /// <param name="source">A <see cref="int"/> instance.</param>
+        /// <param name="action">A <see cref="Action" /> to loop.</param>
         public static void Times(this int source, Action action) {            
             source.Times((i) => { (action ?? emptyAction)(); });
         }
 
+        /// <summary>
+        /// Loops the current <see cref="int" /> times with the given <paramref name="action"/>.
+        /// </summary>
+        /// <param name="source">A <see cref="int"/> instance.</param>
+        /// <param name="action">A <see cref="Action{T}" /> to loop.</param>
         public static void Times(this int source, Action<int> action) {            
             for (int i = 0; i < source; i++) {
                 (action ?? emptyIndexAction)(i);
