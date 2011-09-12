d8688eae4e7d644d629c8977a6681fe46feb049c
Soe Moe <soemoetunlwin@gmail.com>
2011-09-12 17:15:08 +0800

    added IsNumber for string

---
+++ NSupport.Test/StringConversionTest.cs
@@ -0,0 +1,55 @@
+﻿namespace NSupport.Test {
+    using System.Globalization;
+    using Xunit;
+
+    public class StringConversionTest {
+        [Fact]
+        public void Test_IsNumber_for_valid_strings() {
+            var validValues = new string[] { 
+                "1234", "  1234", "1234   ", "   1234  ",
+                "123.34", "+2134", "+23.343", " 1,000", 
+                "-23,000"
+            };
+
+            foreach (var value in validValues) {
+                Assert.True(value.IsNumber(), string.Format("'{0}' should be valid number but now it is not.", value));
+            }
+        }
+
+        [Fact]
+        public void Test_IsNumber_for_invalid_strings() {
+            var invalidValues = new string[] { 
+                "1234sfds", "  1234...", "1234.3243.   ", "   $1234  ",
+                "123.34.0", "-+2134", "++23.343", " 1,000,a", 
+                "-23,000,.."
+            };
+
+            foreach (var value in invalidValues) {
+                Assert.False(value.IsNumber(), string.Format("'{0}' should NOT be valid number but now it is.", value));
+            }
+        }
+
+        [Fact]
+        public void Test_IsNumber_with_NumberStyles_for_valid_strings() {
+            var validValues = new string[] { 
+                "1234", "  1234", "1234   ", "   1234  ",
+                "+2134", "  -23343 "
+            };
+
+            foreach (var value in validValues) {
+                Assert.True(value.IsNumber(NumberStyles.Integer), string.Format("'{0}' should be valid number but now it is not.", value));
+            }
+        }
+
+        [Fact]
+        public void Test_IsNumber_with_NumberStyles_for_invalid_strings() {
+            var invalidValues = new string[] { 
+                "1,000", "  1234.", "1234.32   ", "   $1234  "
+            };
+
+            foreach (var value in invalidValues) {
+                Assert.False(value.IsNumber(NumberStyles.Integer), string.Format("'{0}' should NOT be valid number but now it is.", value));
+            }
+        }
+    }
+}
---
+++ NSupport/StringConversion.cs
@@ -0,0 +1,35 @@
+﻿namespace NSupport {
+    using System;
+    using System.Globalization;
+
+    /// <summary>
+    /// Provides extension methods for <see cref="String"/> for conversion and checking to other types.
+    /// </summary>
+    public static class StringConversion {
+
+        /// <summary>
+        /// Return whether the provided <see cref="String"/> is number.
+        /// It call overload method with this combination of <see cref="NumberStyles"/> (NumberStyles.AllowLeadingWhite | NumberStyles.AllowTrailingWhite | NumberStyles.AllowLeadingSign | NumberStyles.AllowTrailingSign | NumberStyles.AllowDecimalPoint | NumberStyles.AllowThousands;).
+        /// </summary>
+        /// <param name="source">A <see cref="string"/> instance.</param>
+        /// <returns>Returns true if the <see cref="String"/> can be convert to number, otherwise false.</returns>
+        public static bool IsNumber(this string source) {
+            var style = NumberStyles.AllowLeadingWhite | NumberStyles.AllowTrailingWhite | 
+                NumberStyles.AllowLeadingSign | NumberStyles.AllowTrailingSign | 
+                NumberStyles.AllowDecimalPoint | NumberStyles.AllowThousands;
+
+            return source.IsNumber(style);
+        }
+
+        /// <summary>
+        /// Return whether the provided <see cref="String"/> is number with provided <see cref=" NumberStyles"/>.
+        /// </summary>
+        /// <param name="source">A <see cref="string"/> instance.</param>
+        /// <param name="style">A <see cref="NumberStyles" /> to check against <paramref name="source"/>.</param>
+        /// <returns>Returns true if the <see cref="String"/> can be convert to number with provided <see cref=" NumberStyles"/>, otherwise false.</returns>
+        public static bool IsNumber(this string source, NumberStyles style) {
+            decimal parseValue;
+            return decimal.TryParse(source, style, CultureInfo.CurrentCulture, out parseValue);
+        }
+    }
+}
