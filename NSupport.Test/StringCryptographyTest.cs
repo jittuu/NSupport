94e11cc20a668adab9b458dd363e9415a6094967
Soe Moe <soemoetunlwin@gmail.com>
2011-10-12 17:24:28 +0800

    added ToHashString method in string

---
+++ NSupport.Test/StringCryptographyTest.cs
@@ -0,0 +1,51 @@
+﻿namespace NSupport.Test {
+    using System;
+    using Xunit;
+
+    public class StringCryptographyTest {
+        [Fact]
+        public void Test_ToHashString_should_not_return_same_value() {
+            var password = "MySecret";
+            var salt = Guid.NewGuid().ToString("N");
+
+            var hashedPassword = password.ToHashString(salt);
+
+            Assert.NotEqual(hashedPassword, password);
+        }
+
+        [Fact]
+        public void Test_ToHashString_should_not_contain_original_values() {
+            var password = "MySecret";
+            var salt = Guid.NewGuid().ToString("N");
+
+            var hashedPassword = password.ToHashString(salt);
+
+            Assert.DoesNotContain(password, hashedPassword);
+            Assert.DoesNotContain(salt, hashedPassword);
+        }
+
+        [Fact]
+        public void Test_ToHashString_two_different_salt_should_generate_different_hash_string() {
+            var password = "MySecret";
+            var salt = Guid.NewGuid().ToString("N");
+            var anotherSalt = Guid.NewGuid().ToString("N");
+
+            var hashedPassword = password.ToHashString(salt);
+            var anotherHashedPassword = password.ToHashString(anotherSalt);
+
+            Assert.NotEqual(hashedPassword, anotherHashedPassword);
+        }
+
+        [Fact]
+        public void Test_ToHashString_two_different_string_should_generate_different_hash_string() {
+            var password = "MySecret1";
+            var anotherPassword = "MySecret2";
+            var salt = Guid.NewGuid().ToString("N");
+
+            var hashedPassword = password.ToHashString(salt);
+            var hashedAnotherPassword = anotherPassword.ToHashString(salt);
+
+            Assert.NotEqual(hashedAnotherPassword, hashedPassword);
+        }
+    }
+}
---
+++ NSupport/StringCryptography.cs
@@ -0,0 +1,27 @@
+﻿namespace NSupport {
+    using System;
+    using System.Linq;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    /// <summary>
+    /// Provides extension methods to <see cref="string"/> for Crytography services.
+    /// </summary>
+    public static class StringCryptography {
+        /// <summary>
+        /// Hash the given <paramref name="source"/> and <paramref name="salt"/> with SHA256 algorithm.
+        /// </summary>
+        /// <param name="source">An instance of <see cref="String"/>.</param>
+        /// <param name="salt">An instance of <see cref="String"/> for salting before hashing.</param>
+        /// <returns>A hash string.</returns>
+        public static string ToHashString(this string source, string salt) {
+            var sourceBytes = Encoding.UTF8.GetBytes(source);
+            var saltBytes = Encoding.UTF8.GetBytes(salt);
+                        
+            var sourceWithSalt = sourceBytes.Concat(saltBytes).ToArray();
+            var hash = new SHA256Managed().ComputeHash(sourceWithSalt);
+
+            return Convert.ToBase64String(hash);
+        }
+    }
+}
