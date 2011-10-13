92d4ce4f3171ee15be3de91f52adc89a93a5883c
Soe Moe <soemoetunlwin@gmail.com>
2011-10-13 15:59:47 +0800

    added null as invalid values for string#AsDouble extension method tests

--- NSupport.Test/StringToNumberConversionTest.cs
+++ NSupport.Test/StringToNumberConversionTest.cs
@@ -437,7 +437,7 @@
         [Fact]
         public void Test_AsDouble_for_invalid_strings() {
             var invalidValues = new string[] { 
-                "1,..,000", "  1234..", "1234.32.00   ", "   $1234  "
+                null, "1,..,000", "  1234..", "1234.32.00   ", "   $1234  "
             };
 
             foreach (var value in invalidValues) {
@@ -468,6 +468,9 @@
                 { "123.25", NumberStyles.AllowCurrencySymbol },
                 { "+123.25", NumberStyles.AllowDecimalPoint | NumberStyles.AllowCurrencySymbol }
             };
+
+            string nullString = null;
+            Assert.Null(nullString.AsDouble(NumberStyles.AllowDecimalPoint));
 
             foreach (var kv in invalidValues) {
                 Assert.Null(kv.Key.AsDouble(kv.Value));
