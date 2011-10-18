e4d9c5ce5718b6113ceacf45b587b193579966c0
Soe Moe <soemoetunlwin@gmail.com>
2011-10-18 13:11:12 +0800

    added IsNullOrEmpty extension method to string. Closes #1.

---
+++ NSupport.Test/StringAccessTest.cs
@@ -0,0 +1,22 @@
+﻿namespace NSupport.Test {
+    using Xunit;
+
+    public class StringAccessTest {
+        [Fact]
+        public void Test_IsNullOrEmpty_with_null() {
+            string nullString = null;
+            Assert.Equal(true, nullString.IsNullOrEmpty());
+        }
+
+        [Fact]
+        public void Test_IsNullOrEmpty_with_empty_string() {
+            var empty = "";
+            Assert.Equal(true, empty.IsNullOrEmpty());
+        }
+
+        [Fact]
+        public void Test_IsNullOrEmpty_with_string_value() {
+            Assert.Equal(false, "value".IsNullOrEmpty());
+        }
+    }
+}
---
+++ NSupport/StringAccess.cs
@@ -0,0 +1,17 @@
+﻿namespace NSupport {
+
+    /// <summary>
+    /// Provide access methods for <see cref="string"/>.
+    /// </summary>
+    public static class StringAccess {
+
+        /// <summary>
+        /// Check whether given <see cref="string"/> is null or empty.
+        /// </summary>
+        /// <param name="source">An instance of <see cref="string"/>.</param>
+        /// <returns>true if <paramref name="source"/> is null or empty, otherwise false.</returns>
+        public static bool IsNullOrEmpty(this string source) {
+            return string.IsNullOrEmpty(source);
+        }        
+    }
+}
