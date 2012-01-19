3938777a983b84a9130d2d2dc8148ae2fbc1fc88
jittuu <soemoetunlwin@gmail.com>
2012-01-19 18:02:01 +0800

    added IndexOf and LastIndexOf method to array as extension method.

---
+++ NSupport.Test/ArrayAccessTest.cs
@@ -0,0 +1,36 @@
+﻿namespace NSupport.Test {
+    using Xunit;
+    using System;
+
+    public class ArrayAccessTest {
+        [Fact]
+        public void Test_IndexOf_with_null() {
+            int[] nullArray = null;
+            Assert.Throws<ArgumentNullException>(() => nullArray.IndexOf(1));
+        }
+
+        [Fact]
+        public void Test_IndexOf_with_existing_value() {
+            var ints = new[] { 1, 2, 3, 4, 5, 1 };
+            Assert.Equal(0, ints.IndexOf(1));
+        }
+
+        [Fact]
+        public void Test_IndexOf_with_non_existing_value() {
+            var ints = new[] { 1, 2, 3, 4, 5 };
+            Assert.Equal(-1, ints.IndexOf(7));
+        }
+
+        [Fact]
+        public void Test_LastIndexOf_with_existing_value() {
+            var ints = new[] { 1, 2, 3, 4, 5, 1 };
+            Assert.Equal(5, ints.LastIndexOf(1));
+        }
+
+        [Fact]
+        public void Test_LastIndexOf_with_non_existing_value() {
+            var ints = new[] { 1, 2, 3, 4, 5, 1 };
+            Assert.Equal(-1, ints.LastIndexOf(7));
+        }
+    }
+}
---
+++ NSupport/ArrayAccess.cs
@@ -0,0 +1,30 @@
+﻿namespace NSupport {
+    using System;
+
+    /// <summary>
+    /// Provide access methods for <see cref="Array"/>
+    /// </summary>
+    public static class ArrayAccess {
+        /// <summary>
+        /// The zero-based index of the first occurrence of value within the entire <paramref name="source"/>, if found; otherwise, –1.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <param name="source">An instance of <see cref="Array"/>.</param>
+        /// <param name="value">The object to locate in <paramref name="source"/>.</param>
+        /// <returns>The zero-based index of the first occurrence of value within the entire <paramref name="source"/>, if found; otherwise, –1.</returns>
+        public static int IndexOf<T>(this T[] source, T value) {
+            return Array.IndexOf<T>(source, value);
+        }
+
+        /// <summary>
+        /// The zero-based index of the last occurrence of value within the entire <paramref name="source"/>, if found; otherwise, –1.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <param name="source">An instance of <see cref="Array"/>.</param>
+        /// <param name="value">The object to locate in <paramref name="source"/>.</param>
+        /// <returns>The zero-based index of the last occurrence of value within the entire <paramref name="source"/>, if found; otherwise, –1.</returns>
+        public static int LastIndexOf<T>(this T[] source, T value) {
+            return Array.LastIndexOf<T>(source, value);
+        }
+    }
+}
