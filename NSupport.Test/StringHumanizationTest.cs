a4663ad399e6b69d289288c367ea324e3ce2804e
jittuu <soemoetunlwin@gmail.com>
2012-01-19 19:14:31 +0800

    added ToTitleCase method for string

---
+++ NSupport.Test/StringHumanizationTest.cs
@@ -0,0 +1,24 @@
+﻿namespace NSupport.Test {
+    using Xunit;
+    using System;
+
+    public class StringHumanizationTest {
+        [Fact]
+        public void Test_TitleCase_with_null() {
+            string nullValue = null;
+            Assert.Throws<ArgumentNullException>(() => nullValue.ToTitleCase());
+        }
+
+        [Fact]
+        public void Test_TitleCase_with_words() {
+            var value = "war aNd peaCe";
+            Assert.Equal("War And Peace", value.ToTitleCase());
+        }
+
+        [Fact]
+        public void Test_TitleCase_with_all_uppercase() {
+            var value = "WAR AND PEACE";
+            Assert.Equal("War And Peace", value.ToTitleCase());
+        }
+    }
+}
