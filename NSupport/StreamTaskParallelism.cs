7b76a4ac5ae0943cad311d5d60ab664331e111f7
jittuu <soemoetunlwin@gmail.com>
2012-03-23 17:56:49 +0800

    added StreamTaskParallelism#WriteAsync method.

--- NSupport/StreamTaskParallelism.cs
+++ NSupport/StreamTaskParallelism.cs
@@ -25,10 +25,35 @@
         /// <param name="buffer">The buffer to read the data into.</param>
         /// <param name="offset">The byte offset in buffer at which to begin writing data read from the stream.</param>
         /// <param name="count">The maximum number of bytes to read.</param>
-        /// <param name="state">An object containing state information for this asynchronous request.</param>
+        /// <param name="state">An object containing state information for this asynchronous operation.</param>
         /// <returns>The created <see cref="Task{WebResponse}"/> that represents the asynchronous operation.</returns>
         public static Task<int> ReadAsync(this Stream stream, byte[] buffer, int offset, int count, object state) {
             return Task.Factory.FromAsync<byte[], int, int, int>(stream.BeginRead, stream.EndRead, buffer, offset, count, state);
+        }
+
+        /// <summary>
+        /// Begins an asynchronous write operation.
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="buffer">The buffer to write data from.</param>
+        /// <param name="offset">The byte offset in buffer from which to begin writing.</param>
+        /// <param name="count">The maximum number of bytes to write.</param>        
+        /// <returns>The created <see cref="Task{WebResponse}"/> that represents the asynchronous operation.</returns>
+        public static Task WriteAsync(this Stream stream, byte[] buffer, int offset, int count) {
+            return stream.WriteAsync(buffer, offset, count, null);
+        }
+
+        /// <summary>
+        /// Begins an asynchronous write operation.
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="buffer">The buffer to write data from.</param>
+        /// <param name="offset">The byte offset in buffer from which to begin writing.</param>
+        /// <param name="count">The maximum number of bytes to write.</param>
+        /// <param name="state">An object containing state information for this asynchronous operation.</param>
+        /// <returns>The created <see cref="Task{WebResponse}"/> that represents the asynchronous operation.</returns>
+        public static Task WriteAsync(this Stream stream, byte[] buffer, int offset, int count, object state) {
+            return Task.Factory.FromAsync<byte[], int, int>(stream.BeginWrite, stream.EndWrite, buffer, offset, count, state);
         }
     }
 }
