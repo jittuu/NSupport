fc2e867f6e5060cc16ade087fca922d5793daa4d
jittuu <soemoetunlwin@gmail.com>
2012-03-27 14:36:42 +0800

    removed overload method with state object, we don't need that one in Tast-base api

--- NSupport/StreamTaskParallelism.cs
+++ NSupport/StreamTaskParallelism.cs
@@ -15,20 +15,7 @@
         /// <param name="count">The maximum number of bytes to read.</param>
         /// <returns>The created <see cref="Task{WebResponse}"/> that represents the asynchronous operation.</returns>
         public static Task<int> ReadAsync(this Stream stream, byte[] buffer, int offset, int count) {
-            return stream.ReadAsync(buffer, offset, count, null);
-        }
-
-        /// <summary>
-        /// Begins an asynchronous read operation.
-        /// </summary>
-        /// <param name="stream">A <see cref="Stream"/> instance.</param>
-        /// <param name="buffer">The buffer to read the data into.</param>
-        /// <param name="offset">The byte offset in buffer at which to begin writing data read from the stream.</param>
-        /// <param name="count">The maximum number of bytes to read.</param>
-        /// <param name="state">An object containing state information for this asynchronous operation.</param>
-        /// <returns>The created <see cref="Task{WebResponse}"/> that represents the asynchronous operation.</returns>
-        public static Task<int> ReadAsync(this Stream stream, byte[] buffer, int offset, int count, object state) {
-            return Task.Factory.FromAsync<byte[], int, int, int>(stream.BeginRead, stream.EndRead, buffer, offset, count, state);
+            return Task.Factory.FromAsync<byte[], int, int, int>(stream.BeginRead, stream.EndRead, buffer, offset, count, state: null);
         }
 
         /// <summary>
@@ -40,20 +27,7 @@
         /// <param name="count">The maximum number of bytes to write.</param>        
         /// <returns>The created <see cref="Task{WebResponse}"/> that represents the asynchronous operation.</returns>
         public static Task WriteAsync(this Stream stream, byte[] buffer, int offset, int count) {
-            return stream.WriteAsync(buffer, offset, count, null);
-        }
-
-        /// <summary>
-        /// Begins an asynchronous write operation.
-        /// </summary>
-        /// <param name="stream"></param>
-        /// <param name="buffer">The buffer to write data from.</param>
-        /// <param name="offset">The byte offset in buffer from which to begin writing.</param>
-        /// <param name="count">The maximum number of bytes to write.</param>
-        /// <param name="state">An object containing state information for this asynchronous operation.</param>
-        /// <returns>The created <see cref="Task{WebResponse}"/> that represents the asynchronous operation.</returns>
-        public static Task WriteAsync(this Stream stream, byte[] buffer, int offset, int count, object state) {
-            return Task.Factory.FromAsync<byte[], int, int>(stream.BeginWrite, stream.EndWrite, buffer, offset, count, state);
+            return Task.Factory.FromAsync<byte[], int, int>(stream.BeginWrite, stream.EndWrite, buffer, offset, count, state: null);
         }
     }
 }
--- NSupport/WebRequestTaskParallelism.cs
+++ NSupport/WebRequestTaskParallelism.cs
@@ -13,17 +13,7 @@
         /// <param name="request">A <see cref="WebRequest"/> instance.</param>
         /// <returns>The created <see cref="Task{WebResponse}"/> that represents the asynchronous operation.</returns>
         public static Task<WebResponse> GetResponseAsync(this WebRequest request) {
-            return request.GetResponseAsync(null);
-        }
-
-        /// <summary>
-        ///  When overridden in a descendant class, begins an asynchronous request for an Internet resource.
-        /// </summary>
-        /// <param name="request">A <see cref="WebRequest"/> instance.</param>
-        /// <param name="state">An object containing state information for this asynchronous request.</param>
-        /// <returns>The created <see cref="Task{WebResponse}"/> that represents the asynchronous operation.</returns>
-        public static Task<WebResponse> GetResponseAsync(this WebRequest request, object state) {
-            return Task.Factory.FromAsync<WebResponse>(request.BeginGetResponse, request.EndGetResponse, state);
+            return Task.Factory.FromAsync<WebResponse>(request.BeginGetResponse, request.EndGetResponse, state: null);
         }
 
         /// <summary>
@@ -32,17 +22,7 @@
         /// <param name="request">A <see cref="WebRequest"/> instance.</param>
         /// <returns>The created <see cref="Task{Stream}"/> that represents the asynchronous operation.</returns>
         public static Task<Stream> GetRequestStreamAsync(this WebRequest request) {
-            return request.GetRequestStreamAsync(null);
-        }
-
-        /// <summary>
-        /// When overridden in a descendant class, provides an asynchronous version of the <see cref="WebRequest.GetRequestStream"/> method.
-        /// </summary>
-        /// <param name="request">A <see cref="WebRequest"/> instance.</param>
-        /// <param name="state">An object containing state information for this asynchronous request.</param>
-        /// <returns>The created <see cref="Task{Stream}"/> that represents the asynchronous operation.</returns>        
-        public static Task<Stream> GetRequestStreamAsync(this WebRequest request, object state) {
-            return Task.Factory.FromAsync<Stream>(request.BeginGetRequestStream, request.EndGetRequestStream, state);
+            return Task.Factory.FromAsync<Stream>(request.BeginGetRequestStream, request.EndGetRequestStream, state: null);
         }
     }
 }
