08e85885eaab72502046130f5fcd21ecb261d0de
jittuu <soemoetunlwin@gmail.com>
2012-03-27 11:39:29 +0800

    added tests for WebRequestTaskParallelismTest

---
+++ NSupport.Test/WebRequestTaskParallelismTest.cs
@@ -0,0 +1,36 @@
+﻿namespace NSupport.Test {    
+    using Xunit;
+    using System.Net;
+    using System.IO;
+
+    public class WebRequestTaskParallelismTest {
+        [Fact]
+        public void Test_GetResponseAsync() {
+            // Create a request for the URL. 		
+            var request = WebRequest.Create("http://www.contoso.com/default.html");
+
+            request.GetResponseAsync()
+                        .ContinueWith(t => {
+                            using (var stream = t.Result.GetResponseStream())
+                            using (var reader = new StreamReader(stream)) {
+                                var responseFromServer = reader.ReadToEnd();
+                                Assert.NotNull(responseFromServer);
+                                Assert.NotEqual("", responseFromServer);
+                            }
+                        })
+                        .Wait();
+        }
+
+        [Fact]
+        public void Test_GetRequestStreamAsync() {
+            // Create a request for the URL. 		
+            var request = WebRequest.Create("http://www.contoso.com/default.html");
+            request.Method = "POST";
+            request.GetRequestStreamAsync()
+                        .ContinueWith(t => {
+                            Assert.NotNull(t.Result);
+                        })
+                        .Wait();
+        }
+    }
+}
