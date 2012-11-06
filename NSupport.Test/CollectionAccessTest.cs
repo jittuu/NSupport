0b289a2d8a2520cce7d4e6525cf043cae700bf8e
jittuu <soemoetunlwin@gmail.com>
2012-11-06 15:48:03 +0800

    added AddRange method to ICollection<T>

---
+++ NSupport.Test/CollectionAccessTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using System.Collections;
+
+namespace NSupport.Test {
+    public class CollectionAccessTest {
+        [Fact]
+        public void Test_AddRange_with_null_source() {
+            ICollection<int> nullCollection = null;
+            Assert.Throws<ArgumentNullException>(() => nullCollection.AddRange(new int[] { 1 }));
+        }
+
+        [Fact]
+        public void Test_AddRange_with_null_values() {
+            var collection = new IntCollection() { 1, 2, 3, 4, 5 };
+            collection.AddRange(null);
+
+            Assert.Equal(5, collection.Count);
+        }
+
+        [Fact]
+        public void Test_AddRange_with_values() {
+            var collection = new IntCollection() { 1, 2, 3, 4, 5 };
+            collection.AddRange(new int[] { 6, 7 });
+
+            Assert.Equal(7, collection.Count);
+        }
+
+        private class IntCollection : ICollection<int> {
+            List<int> _values = new List<int>();
+
+            public void Add(int item) {
+                _values.Add(item);
+            }
+
+            public void Clear() {
+                throw new NotImplementedException();
+            }
+
+            public bool Contains(int item) {
+                throw new NotImplementedException();
+            }
+
+            public void CopyTo(int[] array, int arrayIndex) {
+                throw new NotImplementedException();
+            }
+
+            public int Count {
+                get { return _values.Count; }
+            }
+
+            public bool IsReadOnly {
+                get { throw new NotImplementedException(); }
+            }
+
+            public bool Remove(int item) {
+                throw new NotImplementedException();
+            }
+
+            public IEnumerator<int> GetEnumerator() {
+                return _values.GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() {
+                return ((IEnumerable)_values).GetEnumerator();
+            }
+        }
+    }
+}
---
+++ NSupport/CollectionAccess.cs
@@ -0,0 +1,30 @@
+﻿namespace NSupport {
+    using System.Collections.Generic;
+
+    /// <summary>
+    ///  Provides extension methods for <see cref="ICollection{T}"/>.
+    /// </summary>
+    public static class CollectionAccess {
+        /// <summary>
+        /// Adds the elements of the specified collection to the end of the <see cref="ICollection{T}"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <param name="source">An instance of <see cref="ICollection{T}"/>.</param>
+        /// <param name="values">The collection whose elements should be added to the end of the <see cref="ICollection{T}"/>. 
+        /// The collection can be null. If null, it won't add anything.
+        /// </param>
+        /// <returns></returns>
+        public static ICollection<T> AddRange<T>(this ICollection<T> source, IEnumerable<T> values) {
+            Guard.ArgumentNotNull("source", source);
+            if (values.IsEmpty()) {
+                return source;
+            }
+
+            foreach (var item in values) {
+                source.Add(item);
+            }
+
+            return source;
+        }
+    }
+}
